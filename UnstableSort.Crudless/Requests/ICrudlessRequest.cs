14c9472cdf5021221771db5a617cd012544f95dd
epicbrownie <epicbrownie@gmail.com>
2019-06-08 00:19:43 -0500

    fix(validation): fix bug preventing validators from running

--- UnstableSort.Crudless/Requests/ICrudlessRequest.cs
+++ UnstableSort.Crudless/Requests/ICrudlessRequest.cs
@@ -1,8 +1,5 @@
-﻿using UnstableSort.Crudless.Mediator;
-
-namespace UnstableSort.Crudless.Requests
+﻿namespace UnstableSort.Crudless.Requests
 {
-    [DoNotValidate]
     public interface ICrudlessRequest
     {
     }
